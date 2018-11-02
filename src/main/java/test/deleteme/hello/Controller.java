173c26ad5d268f8a389c24bffd3a0b62116286d3
Andrey-Vlasov <andrey.vlasov@sap.com>
2018-11-02 18:45:19 -0400

    create maven project with a framwork for springboot

---
+++ src/main/java/test/deleteme/hello/Controller.java
@@ -0,0 +1,15 @@
+package test.deleteme.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class Controller
+{
+	@RequestMapping("hello")
+	public String hello()
+	{
+		return "hello";
+	}
+}
