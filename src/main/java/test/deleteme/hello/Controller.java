0bb6a434cd9ac66224eb7a7e1eea7959c51b6548
Andrey-Vlasov <andrey.vlasov@sap.com>
2018-11-02 20:06:10 -0400

    added apache cayenne database framework and demo

--- src/main/java/test/deleteme/hello/Controller.java
+++ src/main/java/test/deleteme/hello/Controller.java
@@ -1,5 +1,9 @@
 package test.deleteme.hello;
 
+import ca.vastier.depense.generated.model.Item;
+import org.apache.cayenne.ObjectContext;
+import org.apache.cayenne.configuration.server.ServerRuntime;
+import org.apache.cayenne.query.ObjectSelect;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +14,17 @@ public class Controller
 	@RequestMapping("hello")
 	public String hello()
 	{
-		return "hello";
+		final ServerRuntime cayenneRuntime = ServerRuntime.builder().addConfig("cayenne-project.xml").build();
+		final ObjectContext context = cayenneRuntime.newContext();
+
+		Item item = context.newObject(Item.class);
+		item.setName("my favorite item");
+
+		context.commitChanges();
+		item = null;
+
+		item = ObjectSelect.query(Item.class).select(context).get(0);
+
+		return "hello, " + item.getId() + item.getName();
 	}
 }
