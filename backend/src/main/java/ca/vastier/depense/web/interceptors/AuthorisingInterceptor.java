837a163eae08f6d3a518aab1d3dab41beee0f5c9
srtspirit <vlasov_andrei@mail.ru>
2020-11-23 16:41:15 -0500

    fix error with preflight request

--- backend/src/main/java/ca/vastier/depense/web/interceptors/AuthorisingInterceptor.java
+++ backend/src/main/java/ca/vastier/depense/web/interceptors/AuthorisingInterceptor.java
@@ -21,27 +21,32 @@ public class AuthorisingInterceptor extends HandlerInterceptorAdapter
 	public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler)
 			throws Exception
 	{
-		final Secured secured = ((HandlerMethod) handler).getMethod().getAnnotation(Secured.class);
-		if (secured != null)
+		if (handler instanceof HandlerMethod)
 		{
-			final String jwt = request.getHeader("Authorization");
-			final Jwt<Header, Claims> token;
-			try
+			final Secured secured = ((HandlerMethod) handler).getMethod().getAnnotation(Secured.class);
+			if (secured != null)
 			{
-				token = Jwts.parser().setSigningKey("secret".getBytes("UTF-8")).parse(jwt);
-			}
-			catch (final JwtException e)
-			{
-				response.sendError(403, e.getMessage());
-				return false;
-			}
+				final String jwt = request.getHeader("Authorization");
+				final Jwt<Header, Claims> token;
+				try
+				{
+					token = Jwts.parser().setSigningKey("secret".getBytes("UTF-8")).parse(jwt);
+				}
+				catch (final JwtException | IllegalArgumentException e)
+				{
+					response.addHeader("Access-Control-Allow-Origin", "*");
+					response.sendError(403, e.getMessage());
+					return false;
+				}
 
-			final String role = token.getBody().get("role", String.class);
+				final String role = token.getBody().get("role", String.class);
 
-			if(Arrays.stream(secured.allowedRoles()).noneMatch(role::equals))
-			{
-				response.sendError(403, role + " has no access");
-				return false;
+				if (Arrays.stream(secured.allowedRoles()).noneMatch(role::equals))
+				{
+					response.addHeader("Access-Control-Allow-Origin", "*");
+					response.sendError(403, role + " has no access");
+					return false;
+				}
 			}
 		}
 		return true;
